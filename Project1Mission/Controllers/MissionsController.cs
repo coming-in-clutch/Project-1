6869c5ecb60681bc66d4df6278bee3866afe6d89
Julian Sookhoo <jgsookhoo@hotmail.com>
2017-11-07 16:45:32 -0700

    Updated the MIssions

---
+++ Project1Mission/Controllers/MissionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Mission.Controllers
+{
+    public class MissionsController : Controller
+    {
+        // GET: Missions
+        public ActionResult Missions()
+        {
+            return View();
+        }
+    }
+}
